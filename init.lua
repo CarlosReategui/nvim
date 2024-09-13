b4cee6a910627abef1c7a204ff958519737ecb56
Carlos R <carlos.reategui@utec.edu.pe>
2024-09-13 00:21:22 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.o.statuscolumn = "%s%=%l %r"
+
+require("config.lazy")
+
