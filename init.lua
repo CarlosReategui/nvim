7d41a6d878ed8b2148ca46ceef6d7ef734f6ed84
CarlosReategui <carlos.reategui@utec.edu.pe>
2024-09-15 23:24:17 -0500

    tmux navigator config

--- init.lua
+++ init.lua
@@ -9,5 +9,12 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.o.statuscolumn = "%s%=%l %r"
 
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+
 require("config.lazy")
 
