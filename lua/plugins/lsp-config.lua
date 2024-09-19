4daba11776540d570855e26288494e9b071b9670
CarlosReategui <carlos.reategui@utec.edu.pe>
2024-09-19 10:54:32 -0500

    feat: add mason ensure installed

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,7 +2,15 @@ return {
     {
         "williamboman/mason.nvim",
         config = function()
-            require("mason").setup()
+            require("mason").setup({
+                ensure_installed = {
+                    "black",
+                    "isort",
+                    "stylua",
+                    "prettier",
+                    "eslint_d",
+                },
+            })
         end,
     },
     {
