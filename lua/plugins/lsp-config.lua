ae9b499f4c0a19cce54fb1cc8f1aca028eeba708
CarlosReategui <carlos.reategui@utec.edu.pe>
2024-10-09 06:45:55 -0500

    feat(LSP config): Ignore PYLS E501 rule

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,55 +1,64 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                ensure_installed = {
-                    -- "black",
-                    "autopep8",
-                    "isort",
-                    "stylua",
-                    "prettier",
-                    "eslint_d",
-                },
-            })
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "pylsp",
-                    "eslint",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.pylsp.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.eslint.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ensure_installed = {
+					-- "black",
+					"autopep8",
+					"isort",
+					"stylua",
+					"prettier",
+					"eslint_d",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pylsp",
+					"eslint",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+				settings = {
+					pylsp = {
+						plugins = {
+							pycodestyle = {
+                                ignore = { "E501" },
+							},
+						},
+					},
+				},
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+		end,
+	},
 }
